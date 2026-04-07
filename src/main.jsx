f280c0637421ab6368df15a807c9d4be81ed1fbf
KishoreKu <akishorecert@gmail.com>
2026-04-06 20:49:08 -0400

    chore: ignore media assets and clean up tracking

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GhibliAutomation from './GhibliAutomation'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GhibliAutomation />
-  </React.StrictMode>,
-)
